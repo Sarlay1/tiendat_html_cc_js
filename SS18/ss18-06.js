e573b877efddcff4ce7e59a542a8026ce0cd2764
Dotiendat <dat.70.k3@gmail.com>
2025-03-03 00:31:46 +0700

    bai tap ss18

--- SS18/ss18-06.js
+++ SS18/ss18-06.js
@@ -1,12 +1,6 @@
-let year = +prompt("Nhập số năm kinh nghiệm của nhân viên: ")
-if(year >= 6){
-    alert("Quản lý")
-}else if(year<6 && year>=4){
-    alert("Chuyên viên")
-}else if(year<4 && year>=1){
-    alert("Nhân viên có kinh nghiệm")
-}else if(year<1){
-    alert("Mới vào nghề")
-}else{
-    alert("Không có đánh giá hợp lệ")
+let year = parseInt(prompt("Nhập năm bất kì : "));
+if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    alert(year + " là năm nhuận");
+} else {
+    alert(year + " không phải năm nhuận");
 }
